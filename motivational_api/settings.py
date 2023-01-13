1e32bf083265e653f816275648c66042f3801676
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2023-01-13 23:06:45 +0000

    digital ocean

--- motivational_api/settings.py
+++ motivational_api/settings.py
@@ -125,12 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 import os
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'statics')
-]
-STATIC_ROOT=os.path.join(BASE_DIR,'static') 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 
