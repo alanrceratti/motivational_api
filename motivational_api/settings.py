03421f2046e3267f800ba94000c73f24f57c9a8b
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2023-01-13 22:48:25 +0000

    digital ocean

--- motivational_api/settings.py
+++ motivational_api/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 import os
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR,'statics')
 ]
-STATIC_ROOT=os.path.join(BASE_DIR,'workspace/static') 
+STATIC_ROOT=os.path.join(BASE_DIR,'static') 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
